af745793b4402008743746fc9b0db09c44822097
GondekNP <GondekNP@gmail.com>
2022-05-23 23:12:37 -0500

    overnight

--- scripts/convGRU.py
+++ scripts/convGRU.py
@@ -518,7 +518,7 @@ if __name__ == "__main__":
         load_if_exists=True
     )
 
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
